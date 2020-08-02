bc11b9346ae7d392bcb3e5e7d3bdda0ade4aee99
pulkit604 <guppul@gmail.com>
2020-08-02 15:51:22 +0200

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,23 @@
 <template>
-    <login-register v-if="!loggedIn"></login-register>
-    <bot-select v-else></bot-select>
+  <login-register v-if="!loggedIn"></login-register>
+  <bot-select v-else></bot-select>
 </template>
 
 <script>
-    import loginRegister from "../components/loginRegister.vue";
-    import botSelect from "../components/botSelect.vue";
-    export default {
-        name: "HomePage",
-        components: {
-            botSelect,
-            loginRegister,
-        },
-        data() {
-            return {
-                loggedIn: false,
-            };
-        },
+import loginRegister from "../components/loginRegister.vue";
+import botSelect from "../components/botSelect.vue";
+export default {
+  name: "HomePage",
+  components: {
+    botSelect,
+    loginRegister,
+  },
+    computed: {
+      loggedIn: function () {
+        return localStorage.getItem('loggedIn') != "false";
+        }
     }
+}
 </script>
 
 <style scoped>
