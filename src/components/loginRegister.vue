057e8e2633de59bd5d7140a9f8498accc0247284
pulkit604 <guppul@gmail.com>
2020-08-02 15:53:50 +0200

    Update loginRegister.vue

--- src/components/loginRegister.vue
+++ src/components/loginRegister.vue
@@ -1,103 +1,104 @@
 <template>
   <div>
-      <v-btn color="error" dark large @click="showRegister = true">Register</v-btn>
-      <v-btn class="ml-2" color="error" dark large @click="showRegister = false">Login</v-btn>
-      <div v-show="showRegister">
-          <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
-          <v-text-field
-                  v-model="registerForm.name"
-                  label="Name"
-                  required
-          ></v-text-field>
-          <v-text-field
-                  v-model="registerForm.email"
-                  label="Email address"
-                  required
-          ></v-text-field>
-          <v-text-field
-                  v-model="registerForm.pass"
-                  label="Password"
-                  required
-                  type="password"
-          ></v-text-field>
-          <v-btn class="mr-4" @click="doRegister" color="green">Register</v-btn>
-      </div>
-      <div v-show="!showRegister">
-          <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-          <v-text-field
-                  v-model="loginForm.email"
-                  label="Email address"
-                  required
-          ></v-text-field>
-          <v-text-field
-                  v-model="loginForm.pass"
-                  label="Password"
-                  required
-          ></v-text-field>
-          <v-btn class="mr-4" @click="doLogin" color="green">Sign in</v-btn>
+    <v-btn color="error" dark large @click="showRegister = true">Register</v-btn>
+    <v-btn class="ml-2" color="error" dark large @click="showRegister = false">Login</v-btn>
+    <div v-show="showRegister">
+      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+      <v-text-field
+          v-model="registerForm.name"
+          label="Name"
+          required
+      ></v-text-field>
+      <v-text-field
+          v-model="registerForm.email"
+          label="Email address"
+          required
+      ></v-text-field>
+      <v-text-field
+          v-model="registerForm.pass"
+          label="Password"
+          type="password"
+          required
+      ></v-text-field>
+      <v-btn class="mr-4" @click="doRegister" color="green">Register</v-btn>
+    </div>
+    <div v-show="!showRegister">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <v-text-field
+            v-model="loginForm.email"
+            label="Email address"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="loginForm.pass"
+            label="Password"
+            type="password"
+            required
+        ></v-text-field>
+        <v-btn class="mr-4" @click="doLogin" color="green">Sign in</v-btn>
       </div>
   </div>
 </template>
 
 <script>
-    import CONST from '../const.js';
-    export default {
-        name: "loginRegister",
-        data() {
-            return {
-            loggedIn: false,
-            showRegister: false,
-            authToken: '',
-            loginForm: {
-              email: 'guppul@gmail.com',
-              pass: '123321',
-            },
-            registerForm: {
-                name: 'Pulkit Gupta',
-                email: 'guppul@gmail.com',
-                pass: '123321'
-            },
-            };
-        },
-        methods: {
-            doRegister() {
-             fetch(CONST.API_HOST + '?create_account',
-                 { method : 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body   : JSON.stringify({
-                            name: this.registerForm.name,
-                            email: this.registerForm.email,
-                            pass: this.registerForm.pass
-                        })
-                 })
-                 .then(response => response.json())
-                    .then(response => {
-                        localStorage.setItem('loginToken', response.token);
-                        this.$route.push({ path: '/' });
-                        this.loggedIn = true;
-                    })
-             .catch(error => {console.log(error);})
-             ;
-            },
-            doLogin() {
-             fetch(CONST.API_HOST + '?login',  {
-                    method : 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body   : JSON.stringify({
-                    name: this.registerForm.name,
-                    email: this.registerForm.email,
-                    pass: this.registerForm.pass
-                })
-             })
-                 .then(response => response.json())
-                 .then(response => {
-                     localStorage.setItem('loginToken', response.token);
-                     this.$route.push({ path: '/' });
-                     this.loggedIn = true;
-                 });
-            },
-        },
-    }
+import CONST from '../const.js';
+import router from '../router.js';
+export default {
+  name: "loginRegister",
+  data() {
+    return {
+      loggedIn: false,
+      showRegister: false,
+      authToken: '',
+      loginForm: {
+        email: 'guppul@gmail.com',
+        pass: '123321',
+      },
+      registerForm: {
+        name: 'Pulkit Gupta',
+        email: 'guppul@gmail.com',
+        pass: '123321'
+      },
+    };
+  },
+  methods: {
+    doRegister() {
+      fetch(CONST.API_HOST + '?create_account', { method : 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body   : JSON.stringify({
+          name: this.registerForm.name,
+          email: this.registerForm.email,
+          pass: this.registerForm.pass
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        localStorage.setItem('loginToken', response.accessToken);
+        router.push({ path: '/' });
+        location.reload();
+      })
+      .catch(error => {console.log(error);})
+      ;
+    },
+    doLogin() {
+      fetch(CONST.API_HOST + '?login',  {
+        method : 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body   : JSON.stringify({
+          name: this.registerForm.name,
+          email: this.registerForm.email,
+          pass: this.registerForm.pass
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        localStorage.setItem('loginToken', response.accessToken);
+        router.push({ path: '/' });
+        location.reload();
+      });
+    },
+  },
+}
 </script>
 
 <style scoped>
